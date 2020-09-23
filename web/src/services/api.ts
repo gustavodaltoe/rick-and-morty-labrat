2e12e906f5b460ea496c692fb5526b7ce05d3642
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-09-23 15:41:21 -0300

    fix: vercel env

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URI || 'http://localhost:3333/',
+  baseURL: process.env.REACT_APP_API_URI || 'http://localhost:3333/',
 });
 
 const getHumans = () => api.get('/humans');
